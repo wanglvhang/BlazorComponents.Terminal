0e1c0853cf4c4d3ec2bf3b7f5b34da3f242363a6
wanglvhang <wanglvhang@qq.com>
2023-08-12 13:22:15 +0800

    add version

--- BlazorComponents.Terminal/UtilityExtentions.cs
+++ BlazorComponents.Terminal/UtilityExtentions.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
